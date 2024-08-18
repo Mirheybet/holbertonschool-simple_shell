d8daf53819765b20019697d2217c32269b04482b
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-08-18 16:35:31 +0400

    simple_shell first commit

---
+++ shell.c
@@ -0,0 +1,31 @@
+#define PATH "PATH="
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#define MAX_LEN 100
+#define PROMPT ">> "
+
+
+int main (void){
+	char command[MAX_LEN];
+
+	while (1)
+	{
+		write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+		if (fgets(command, MAX_LEN, stdin) == NULL)
+		{
+			if (feof(stdin))
+				break;
+			perror("Error reading command");
+			continue;
+		}
+
+		command[strcspn(command, "\n")] = '\0';
+	}
+	return (0);
+}
