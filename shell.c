9e3da3c87a2190406b9c4ef1eb06d11292b9e6df
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-08-18 20:04:37 +0400

    exucute command added

--- shell.c
+++ shell.c
@@ -18,10 +18,33 @@ void execute_commands(char **args, char *path)
 {
 	pid_t pid;
 	int ststaus;
+
+
 	pid = fork();
 	if (pid == -1)
 	{
-		p
+		perror("fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			free(path);
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+		{
+			free(path);
+			perror("wait failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
 
 /**
  * devide_commands - commands processor func
