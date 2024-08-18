9f33ddedf61d8793658bd4ad3c313274f81a041e
Orkhanibrahim357 <9418@holbertonstudents.com>
2024-08-18 16:38:26 +0400

    I filled in the main.h file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAINH
+#define MAINH
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <dirent.h>
+
+#define MAX_LEN 100
+#define PROMPT ">> "
+#define PATH "PATH="
