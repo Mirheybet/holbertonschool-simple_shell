c5d61b5978d96c22f697fa23cbda866b4f37a118
Mirheybet <mirheybet.hesenov@gmail.com>
2024-08-23 20:38:34 +0000

    execcomm-upd2

--- execomm.c
+++ execomm.c
@@ -2,31 +2,33 @@
 
 void execute_command(char **args, char *path)
 {
-        pid_t pid;
-        int status;
+	pid_t pid;
+	int status;
 
-        pid = fork();
-        if (pid == -1)
-        {
-                perror("fork failed");
-                exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-                if (execve(path, args, environ) == -1)
-                {
-                        free(path);
-                        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-                        exit(EXIT_FAILURE);
-                }
-        }
-        else
-        {
-                if (wait(&status) == -1)
-                {
-                        free(path);
-                        perror("wait failed");
-                        exit(EXIT_FAILURE);
-                }
-        }
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork failed");
+		exit(EXIT_FAILURE);
+	}
+
+	else if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			free(path);
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+		{
+			free(path);
+			perror("wait failed");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
