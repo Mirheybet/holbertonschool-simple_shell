3a626311c3823553069a904626a689883c802016
Mirheybet <mirheybet.hesenov@gmail.com>
2024-08-23 20:43:20 +0000

    handle_comm-upd1

--- shell.c
+++ shell.c
@@ -155,25 +155,6 @@ void handle_command(char *u_command)
 	execute_command(args, path);
 	free(path);
 }
-
-void handle_commands_array(char **commands_array)
-{
-	int a = 0;
-	char *command;
-	if (strcmp(commands_array[a], "exit") == 0)
-		exit(0);
-	else if (strcmp(commands_array[a], "env") == 0)
-		print_env();
-	else
-		while (commands_array[a] != NULL)
-		{
-			command = commands_array[a];
-			if (strcmp(command, "exit") == 0 && a > 0)
-				exit(2);
-			handle_command(command);
-			a++;
-		}
-}
 /**
  * main - main func to process all functions
  * Return: integer
