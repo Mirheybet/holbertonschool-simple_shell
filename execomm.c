a42e0f38b44403b336e191c1b862403fd8e1e616
Mirheybet <mirheybet.hesenov@gmail.com>
2024-08-23 20:34:27 +0000

    execcommfile-upd1

---
+++ execomm.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void execute_command(char **args, char *path)
+{
+        pid_t pid;
+        int status;
+
+        pid = fork();
+        if (pid == -1)
+        {
+                perror("fork failed");
+                exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+                if (execve(path, args, environ) == -1)
+                {
+                        free(path);
+                        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+                        exit(EXIT_FAILURE);
+                }
+        }
+        else
+        {
+                if (wait(&status) == -1)
+                {
+                        free(path);
+                        perror("wait failed");
+                        exit(EXIT_FAILURE);
+                }
+        }
+}
